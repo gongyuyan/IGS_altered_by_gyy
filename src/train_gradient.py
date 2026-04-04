88ed49b5178430dfc60e392ca3ea183a07b1211b
gong yuyan <gongyuyan@stu.scu.edu.cn>
2026-04-04 16:17:10 +0800

    Update train_gradient.py

--- src/train_gradient.py
+++ src/train_gradient.py
@@ -156,8 +156,8 @@ def Gradient_iteration(args):
         meta_mask = torch.where(saliency_two_avg < threshold_two, 0, 1).cpu().numpy()
     elif args.metaMask_Sum:
         gradient_matrix = saliency_unified_0 + saliency_unified_1 #添加4.4.1
-        gradient_matrix = gradient_matrix / gradient_matrix.max() #添加4.4.2
-        saliency_two_sum = 0.01 * gradient_matrix - lambda_degree * degree_matrix + lambda_sim * sim_matrix  #添加4.4.3
+        # gradient_matrix = gradient_matrix / gradient_matrix.max() #添加4.4.2
+        saliency_two_sum = gradient_matrix - lambda_degree * degree_matrix + lambda_sim * sim_matrix  #添加4.4.3
         # saliency_two_sum = saliency_unified_0 + saliency_unified_1 - lambda_degree * degree_matrix + lambda_sim * sim_matrix   #⭐添加3.18.4
         saved_saliency_map = saliency_two_sum.cpu()
         threshold_two = torch.quantile(saliency_two_sum, args.prune_threshold).item()
